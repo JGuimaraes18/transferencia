af4b93fbe41f86351e945175cdbac88bd56f24da
JGuimaraes18 <78107623+JGuimaraes18@users.noreply.github.com>
2021-04-20 14:20:19 -0300

    Add files via upload

---
+++ aqua_terra_npp_noaa20.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+servidor=$1
+passagem=$2
+satelite=$3
+sensor=$4
+
+servidor=`echo $servidor | tr [A-Z] [a-z]` 
+ingest="$servidor.dgi.inpe.br"
+usuario='cbers'
+
+path_servidor='/home/cbers/bin'
+path_origem="/mnt/$servidor/$passagem"
+cmd_raw="$path_servidor/converte_CB4_to_AQUA_RAW.sh"
+
+cd $path_origem
+
+DRD=`ls CBERS*`
+ano_mes=`echo $DRD |cut -d '_' -f5-6`
+data_hora=`echo $DRD |cut -d '_' -f5-`
+path_destino='/home/transfoper/TEMP-TESTE-PASSAGEM/'$satelite/$ano_mes/$sensor
+mkdir -p $path_destino
+
+case $satelite in
+	AQUA)
+		formato='CADU'
+		;;
+	TERRA)
+		formato='CADU'
+		;;
+	NPP)
+		formato='RAW'
+		;;
+	NOAA20)
+		formato='RAW'
+		;;
+esac
+
+RAW=$satelite'_'$formato'_'$data_hora'_CP5'
+
+ssh $usuario@$ingest << EOF
+$cmd_raw $passagem $RAW $DRD
+EOF
+
+cp $path_origem/$RAW $path_destino
+
+exit
