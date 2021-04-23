c48ccdab4ab4b4d825feb00042a588085cfdbfc3
JGuimaraes18 <78107623+JGuimaraes18@users.noreply.github.com>
2021-04-23 13:33:05 -0300

    Update amazonia.sh

--- amazonia.sh
+++ amazonia.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -x 
+#!/bin/bash 
 
 servidor=$1
 passagem=$2
 sensor=$3
 
-servidor=`echo $servidor | tr [A-Z] [a-z]`
+servidor=`echo $servidor | tr [A-Z] [a-z]` 
 ingest="$servidor.dgi.inpe.br"
 usuario='cbers'
 
@@ -14,7 +14,6 @@ mes=`echo $passagem |cut -d '-' -f3`
 path_servidor='/home/cbers/bin'
 path_origem="/mnt/$servidor/$passagem"
 path_destino='/Level-0/AMAZONIA1/'$ano'_'$mes/$sensor
-
 mkdir -p $path_destino
 
 cmd_raw="$path_servidor/converte_CB4_to_AMZ1_RAW.sh"
@@ -30,7 +29,6 @@ EOF
 
 if [ $servidor == 'solaria' ]
 then
-        cp $path_origem'/AMAZONIA'* $path_destino
+	cp $path_origem'/AMAZONIA'* $path_destino
 fi
-
 exit
