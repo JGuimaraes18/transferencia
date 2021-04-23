530973cafda5b881ee33b7e5f5da676451fadfc4
JGuimaraes18 <78107623+JGuimaraes18@users.noreply.github.com>
2021-04-23 13:30:25 -0300

    Update cbers4.sh

--- cbers4.sh
+++ cbers4.sh
@@ -4,7 +4,7 @@ servidor=$1
 passagem=$2
 sensor=$3
 
-servidor=`echo $servidor | tr [A-Z] [a-z]`
+servidor=`echo $servidor | tr [A-Z] [a-z]` 
 ingest="$servidor.dgi.inpe.br"
 usuario='cbers'
 
@@ -15,13 +15,9 @@ path_origem="/mnt/$servidor/$passagem"
 path_destino='/Level-0/CBERS4/'$ano'_'$mes/$sensor
 
 mkdir -p $path_destino
-
 cd $path_origem
-
 CB4_ORIG=`ls CBERS*`
-
 CB4=$CB4_ORIG'_CP5'
-
 cp $CB4_ORIG $path_destino/$CB4
 
 exit
