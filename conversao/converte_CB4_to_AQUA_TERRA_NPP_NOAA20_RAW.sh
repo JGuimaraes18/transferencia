f70ff2fe7255e049ae8128e54476243d1b61feda
JGuimaraes18 <78107623+JGuimaraes18@users.noreply.github.com>
2021-04-29 10:35:56 -0300

    Update converte_CB4_to_AQUA_TERRA_NPP_NOAA20_RAW.sh

--- conversao/converte_CB4_to_AQUA_TERRA_NPP_NOAA20_RAW.sh
+++ conversao/converte_CB4_to_AQUA_TERRA_NPP_NOAA20_RAW.sh
@@ -1,18 +1,11 @@
 #!/bin/bash 
-
-########################################################################################
-# Script que realiza a conversao de dados no formato DRD_CBERS-4 para RAW_AQUA via ms3 #
-# Autor: Joao Guimaraes                                              #
-# Data: 19/04/2021                                                                     #
-########################################################################################
-
 # Define variaveis locais #
 passagem=$1
+RAW=$2
 DRD=$3
-path_passagem="/mnt/solaria/disco1/A1/$passagem"
-path_raw="/TEMP/$2"
+path_passagem="/mnt/solaria/$passagem"
+path_exe="/TEMP"
 
-
-cd $path_passagem
-/usr/local/bin/station_drd_to_raw $path_exe/$DRD $path_raw
+cp $path_passagem/$DRD $path_exe
+/usr/local/bin/station_drd_to_raw $path_exe/$DRD $path_exe/$RAW
 exit
