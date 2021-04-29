82d65ea490084631fa179185820846e6e3727904
JGuimaraes18 <78107623+JGuimaraes18@users.noreply.github.com>
2021-04-29 10:38:25 -0300

    Update AMAZONIA.sh

--- conversao/AMAZONIA.sh
+++ conversao/AMAZONIA.sh
@@ -15,12 +15,10 @@ path_destino='/Level-0/AMAZONIA1/'$ano'_'$mes/$sensor
 cmd_raw="$path_exe/converte_CB4_to_AMZ1_RAW.sh"
 cmd_drd="$path_exe/converte_CB4_to_AMZ1_DRD.sh"
 
-$cmd_raw $passagem $servidor
-
-$cmd_drd $passagem $servidor
-
 if [ $servidor == 'solaria' ]
 then
+	$cmd_raw $passagem $servidor
+	$cmd_drd $servidor
 	cd $path_origem
 	DADO=`ls AMAZONIA*`
 	cp $DADO $path_destino
