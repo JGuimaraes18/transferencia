2185f6e91b0a0ddadd369eb6ce20a0a64791f613
JGuimaraes18 <78107623+JGuimaraes18@users.noreply.github.com>
2021-04-29 10:36:23 -0300

    Update converte_CB4_to_AMZ1_DRD.sh

--- conversao/converte_CB4_to_AMZ1_DRD.sh
+++ conversao/converte_CB4_to_AMZ1_DRD.sh
@@ -1,27 +1,15 @@
 #!/bin/bash 
-
-########################################################################################
-# Script que realiza a conversao de dados no formato DRD_CBERS-4 para DRD_AMZ1 via ms3 #
-# Autor: Reuel Junqueira / Joao Guimaraes                                              #
-# Data: 17/03/2021                                                                     #
-########################################################################################
 # Define variaveis locais #
-passagem=$1
-ingestora=$2
-path_passagem="/mnt/$ingestora/disco1/A1/$passagem"
+ingestora=$1
 path_exe="/TEMP/$ingestora"
 antena='CP5'
 
-cd $path_passagem
-CB4_DRD=`ls -1 CBERS_4_*DRD*`		 
-data_hora=`echo $CB4_DRD |cut -d '_' -f5- `
-ano_mes=`echo $data_hora |cut -d '_' -f1-2`
-
-# Definindo o nome dos arquivos
-AMZ_RAW='AMAZONIA_1_WFI_RAW_'$data_hora'_'$antena
-AMZ_DRD='AMAZONIA_1_WFI_DRD_'$data_hora'_'$antena
+cd $path_exe
+AMZ_RAW=`ls -1 AMAZONIA_1_WFI_RAW*`		 
+data_hora=`echo $AMZ_RAW |cut -d '_' -f5- `
+AMZ_DRD='AMAZONIA_1_WFI_DRD_'$data_hora
 		
-/usr/local/bin/station_raw_to_drd  $path_exe/$AMZ_RAW AMAZONIA 1 WFI  $path_exe/$AMZ_DRD
+/usr/local/bin/station_raw_to_drd  $AMZ_RAW AMAZONIA 1 WFI  $AMZ_DRD
 
-rm $path_exe/$AMZ_RAW
+rm $AMZ_RAW
 exit
