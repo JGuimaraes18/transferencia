bfb81e03ec106455f17c1d8b9cf99f3240c4e178
JGuimaraes18 <78107623+JGuimaraes18@users.noreply.github.com>
2021-04-29 14:08:27 -0300

    Update converte_CB4_to_AQUA_TERRA_NPP_NOAA20_RAW.sh

--- conversao/converte_CB4_to_AQUA_TERRA_NPP_NOAA20_RAW.sh
+++ conversao/converte_CB4_to_AQUA_TERRA_NPP_NOAA20_RAW.sh
@@ -4,8 +4,9 @@ passagem=$1
 RAW=$2
 DRD=$3
 path_passagem="/mnt/solaria/$passagem"
-path_exe="/TEMP"
+path_exe="/TEMP/solaria"
 
 cp $path_passagem/$DRD $path_exe
+cd $path_exe
 /usr/local/bin/station_drd_to_raw $path_exe/$DRD $path_exe/$RAW
 exit
