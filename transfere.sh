1c26b5d4d98ed183a802f441488327d6dc356de7
JGuimaraes18 <78107623+JGuimaraes18@users.noreply.github.com>
2021-04-29 10:44:20 -0300

    Update transfere.sh

--- transfere.sh
+++ transfere.sh
@@ -31,63 +31,68 @@ do
 			defineMascara
 			listaPassagens	
 
-			for passagemIngestora in $lista_passagens
-                        do
-				if [ "$sistemaIngestao" == 'DARTCOM' ]
-                        	then
-					acertaPadraoIngestoraDartcom	
-                        	fi	
-
-                              	formataDataPassagem "$passagemIngestora"
-                               	comparaHorarioPassagem $data_completa $data_compara_passagem
-
-				if [ $resultado -ge -5  ] && [ $resultado -le 10 ] 
-				then
+			if [ -z $hora_lista_passagem ]
+			then
+				continue
+			else
+				for passagemIngestora in $lista_passagens
+	                        do
 					if [ "$sistemaIngestao" == 'DARTCOM' ]
-					then
-						cd $ponto_de_montagem/$sat/$data/"$passagemIngestora"
-					else
-						cd $ponto_de_montagem/$passagemIngestora
-					fi
-				
-					defineQuantidadePassagens
-					atualizaBD
+	                        	then
+									acertaPadraoIngestoraDartcom	
+	                        	fi	
 
-					if [ $antena == 'CP5' ]
+	                              	formataDataPassagem "$passagemIngestora"
+	                               	comparaHorarioPassagem $data_completa $data_compara_passagem
+
+					if [ $resultado -ge -5  ] && [ $resultado -le 10 ] 
 					then
-						if [ "$satelite" == 'CBERS4' ]
-                	        	        then
-							ssh $usuario@$convert << EOF
-							$path_exe/$satelite.sh $servidor $ingest_data $sensor
+						if [ "$sistemaIngestao" == 'DARTCOM' ]
+						then
+							cd $ponto_de_montagem/$sat/$data/"$passagemIngestora"
+						else
+							cd $ponto_de_montagem/$passagemIngestora
+						fi
+				
+						defineQuantidadePassagens
+						atualizaBD
+
+						if [ $antena == 'CP5' ]
+						then
+							if [ "$satelite" == 'CBERS4' ]
+	                	        	        then
+								ssh $usuario@$convert << EOF
+								$path_exe/$satelite.sh $servidor $ingest_data $sensor
 EOF
-                               		        fi
-						if [ "$satelite" == 'AMAZONIA 1' ] 
+	                               		        fi
+							if [ "$satelite" == 'AMAZONIA 1' ] 
+							then
+								ssh $usuario@$convert << EOF
+								$path_exe/AMAZONIA.sh $servidor $ingest_data 
+EOF
+							fi
+							if [ "$satelite" == 'AQUA' ] || [ "$satelite" == 'TERRA' ] || [ "$satelite" == 'NPP' ] || [ "$satelite" == 'NOAA20' ]
+	               		                        then
+								ssh $usuario@$convert << EOF
+	                	       	                        $path_exe/$satelite-CP5.sh $ingest_data
+EOF
+							fi
+							if [ "$satelite" == 'CBERS 4A' ]
+	        	       	                        then
+								ssh $usuario@$convert << EOF
+								$path_exe/CB4A.sh $servidor $ingest_data $sensor $ponto_de_montagem
+EOF
+							fi
+						fi
+						if [ $antena == 'CP6' ] || [ $antena == 'CP1' ] 
 						then
 							ssh $usuario@$convert << EOF
-							$path_exe/AMAZONIA.sh $servidor $ingest_data 
-EOF
-						fi
-						if [ "$satelite" == 'AQUA' ] || [ "$satelite" == 'TERRA' ] || [ "$satelite" == 'NPP' ] || [ "$satelite" == 'NOAA20' ]
-               		                        then
-							ssh $usuario@$convert << EOF
-                	       	                        $path_exe/$satelite-CP5.sh $ingest_data
-EOF
-						fi
-						if [ "$satelite" == 'CBERS 4A' ]
-        	       	                        then
-							ssh $usuario@$convert << EOF
-							$path_exe/CB4A.sh $servidor $ingest_data $sensor $ponto_de_montagem
+							$path_exe/$satelite-$antena.sh $data_hora $ponto_de_montagem
 EOF
 						fi
 					fi
-					if [ $antena == 'CP6' ] || [ $antena == 'CP1' ] 
-					then
-						ssh $usuario@$convert << EOF
-						$path_exe/$satelite-$antena.sh $data_hora $ponto_de_montagem
-EOF
-					fi
-				fi
-			done
+				done
+			fi
 		fi
 	fi
 done
