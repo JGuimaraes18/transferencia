705b402d9c1bdbf42659730a2963aaba83b3f3c7
JGuimaraes18 <78107623+JGuimaraes18@users.noreply.github.com>
2021-09-11 19:44:40 -0300

    Adiciona envio de email e transferência para DPI

--- conversao/AMAZONIA.sh
+++ conversao/AMAZONIA.sh
@@ -1,12 +1,16 @@
-#!/bin/bash 
+#!/bin/bash
+source email.sh
 
 servidor=$1
 passagem=$2
 sensor='WFI'		
-contatos='joao.guimaraes@inpe.br'
 servidor=`echo $servidor | tr [A-Z] [a-z]` 
 ano=`echo $passagem |cut -d '-' -f2`
 mes=`echo $passagem |cut -d '-' -f3`
+
+ftp='HOST'
+user='USER'
+dirDPI='/cdsr/VPN/AMAZONIA'
 
 path_exe='/home/transfoper/bin'
 path_origem="/TEMP/$servidor"
@@ -15,15 +19,17 @@ path_destino='/Level-0/AMAZONIA1/'$ano'_'$mes/$sensor
 cmd_raw="$path_exe/converte_CB4_to_AMZ1_RAW.sh"
 cmd_drd="$path_exe/converte_CB4_to_AMZ1_DRD.sh"
 
-if [ $servidor == 'solaria' ]
-then
-	$cmd_raw $passagem $servidor
-	$cmd_drd $servidor
-	cd $path_origem
-	DADO=`ls AMAZONIA*`
-	cp $DADO $path_destino
-	echo "Dado $DADO disponível no Level-0 para processamento" | mail -s "Dado $DADO Disponível" $contatos	
-fi
+$cmd_raw $passagem $servidor
+$cmd_drd $servidor
+cd $path_origem
+DADO=`ls AMAZONIA*`
+mkdir -p $path_destino
+cp $DADO $path_destino
+
+enviaEmail $DADO
+
+scp $DADO $user@$ftp:@$dirDPI
+
 cd $path_origem
 rm AMAZONIA*
 exit
