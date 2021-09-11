8ef832639b0e7db4da8ed2ea0d441eef54a05737
JGuimaraes18 <78107623+JGuimaraes18@users.noreply.github.com>
2021-09-11 20:07:45 -0300

    Funcoes de envio de email

---
+++ conversao/email.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x
+
+contatos='joao.guimaraes@inpe.br, cdsr@inpe.br, marilia.moura@inpe.br'
+
+function enviaEmail(){
+DADO=$1
+
+echo "Dado $DADO disponível no Level-0 para processamento" | mail -s "Dado $DADO Disponível" $contatos
+}
+ 
+function verificaEmailIgual(){
+echo "Prezado(a) Operador(a)," >> emailOP
+echo "                                  "  >> emailOP
+echo "O arquivo $DADO esta com o Checksum igual em Cachoeira Paulista.."  >> emailOP
+echo "                                  " >> emailOP
+echo "                                  " >> emailOP
+echo "Obrigado." >> emailOP
+echo "                                  " >> emailOP
+echo "SUPORTE" >> emailOP
+echo "                                  " >> emailOP
+echo "                                  " >> emailOP
+
+
+assunto="CHECKSUM IGUAL - $1"
+mail -s "$assunto" $contatos < emailOP
+rm emailOP
+}
+
+function verificaEmailDiferenca(){
+echo "Prezado(a) Operador(a)," >> emailOP
+echo "                                  "  >> emailOP
+echo "O arquivo $DADO esta com o Checksum diferente"  >> emailOP
+echo "                                  " >> emailOP
+echo "Obrigado." >> emailOP
+echo "                                  " >> emailOP
+echo "SUPORTE" >> emailOP
+echo "                                  " >> emailOP
+echo "                                  " >> emailOP
+
+
+assunto="CHECKSUM IGUAL - $1"
+mail -s "$assunto" $contatos < emailOP
+rm emailOP
+}
+
